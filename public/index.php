25aceeeb29aab25356d5e0241d1d7bbfef31c376
enikeishik <enikeishik@gmail.com>
2018-12-22 09:43:36 +0300

    feat: cleanup

--- public/index.php
+++ public/index.php
@@ -5,21 +5,21 @@ use \Ufo\Core\Config;
 use \Ufo\Core\Debug;
 use \Ufo\Core\App;
 
-$_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/..';
-//var_dump($_SERVER['DOCUMENT_ROOT']);
+if (empty($_SERVER['DOCUMENT_ROOT'])) {
+    $_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/..';
+}
+
 $config = new Config();
 $debug = new Debug();
 $app = new App($config, $debug);
 $config->routeStorageData = require '../data/RouteStorageData.php';
 $config->widgetsStorageData = require '../data/WidgetsStorageData.php';
 
-//$_GET['path'] = '/qwe/asd';
+
 $debug->trace('execute');
 $app->execute();
-// $result = $app->compose($app->parse());
-// $result->getHeaders();
-// $result->getContent();
 $debug->traceEnd();
+
 echo 
     PHP_EOL . 
     'execution time: ' . round(100 * $debug->getExecutionTime(), 2) . ' ms; ' . 
