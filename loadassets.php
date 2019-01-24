3d8781ee10a182e59a68b038cac572b07cb7ec41
enikeishik <enikeishik@gmail.com>
2019-01-24 11:23:33 +0300

    feat: xcopy logic in assets loader

--- loadassets.php
+++ loadassets.php
@@ -14,11 +14,48 @@ if (empty($argv[1])) {
     exit('Usage: php -f loadassets.php vendor/module');
 }
 
+define('MKDIR_MODE', 0755);
+
+/**
+ * @param string $src
+ * @param string $dst
+ * @throws \Exception
+ */
+function xcopy($src, $dst) {
+    $files = @scandir($src);
+    if (!$files) {
+        exit('Call scandir for `' . $src . '` failed');
+    }
+    if (!file_exists($dst)) {
+        if (!@mkdir($dst, MKDIR_MODE, true)) {
+            exit('Call mkdir for `' . $dst . '` failed');
+        }
+    }
+    
+    foreach ($files as $file) {
+        if ('.' == $file || '..' == $file) {
+            continue;
+        }
+        $srcItm = $src . '/' . $file;
+        $dstItm = $dst . '/' . $file;
+        if (is_dir($srcItm)) {
+            if (!@mkdir($dstItm, MKDIR_MODE, true)) {
+                exit('Call mkdir for `' . $dstItm . '` failed');
+            }
+            xcopy($srcItm, $dstItm);
+        } else {
+            if (!@copy($srcItm, $dstItm)) {
+                exit('Call copy(' . $srcItm . ', ' . $dstItm . ') failed');
+            }
+        }
+    }
+}
+
 $package = $argv[1];
 
-$src = 'vendor/' . $package . '/composer.json';
-$trg = 'resources/templates/default/' . $package . '.json';
+$src = __DIR__ . '/vendor/' . $package . '/resources';
+$trg = __DIR__ . '/resources/templates/default/' . $package;
 
 if (file_exists($src) && !file_exists($trg)) {
-    copy($src, $trg);
+    xcopy($src, $trg);
 }
